aeeb44f6843e5ab567b80e17d994e70e5e238207
Raffinatofabrics <raffinato.it@gmail.com>
2026-04-30 11:11:06 +0800

    fix: 切换到 PostgreSQL 数据库，删除临时密码重置功能

--- crm/settings.py
+++ crm/settings.py
@@ -88,13 +88,14 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
