87cd455f1d88c34f94aa0831eea8ce0d748fab9d
Raffinatofabrics <raffinato.it@gmail.com>
2026-04-30 12:26:25 +0800

    修复部门归属

--- customers/models.py
+++ customers/models.py
@@ -737,3 +737,22 @@ class UnsubscribeBlacklist(models.Model):
     
     def __str__(self):
         return self.email
+
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """用户创建时自动创建 UserProfile"""
+    if created:
+        UserProfile.objects.get_or_create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """保存用户时保存 UserProfile"""
+    if hasattr(instance, 'userprofile'):
+        instance.userprofile.save()
